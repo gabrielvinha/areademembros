9cd000e2fa8925e03bd2cf4db20d880a919a8c38
gabrielvinha <64176098+gabrielvinha@users.noreply.github.com>
2025-10-16 19:04:36 +0200

    Updated ModulesSection.tsx

--- src/components/ModulesSection.tsx
+++ src/components/ModulesSection.tsx
@@ -7,6 +7,8 @@ import { Lock } from 'lucide-react';
 interface ModulesSectionProps {
   unlockedModules: Set<string>;
   onUnlock: (moduleId: string) => void;
+  daysRemaining?: number; const ModulesSection: React.FC<ModulesSectionProps> = ({ unlockedModules, onUnlock, daysRemaining = 0 }) => {
+
 }
 
 const ModulesSection: React.FC<ModulesSectionProps> = ({ unlockedModules, onUnlock }) => {
